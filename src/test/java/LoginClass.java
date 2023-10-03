e9cc1e2db992c3ed225e58fd3c2ffbeb6107e560
Anusuya-Balakrishnan <anusuyabalakrishnan01@gmail.com>
2023-10-03 16:55:50 +0530

    3.10.2023

--- src/test/java/LoginClass.java
+++ src/test/java/LoginClass.java
@@ -2,7 +2,9 @@ import java.sql.Time;
 import java.time.Duration;
 import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.By.ByXPath;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -10,9 +12,10 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import org.w3c.dom.html.HTMLAnchorElement;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
+import java.util.*;
 @Test
 public class LoginClass {
 	
@@ -81,6 +84,70 @@ public class LoginClass {
 		}
 		
 		
+//		click notifications
+//		WebElement notificationElement=wait.until(ExpectedConditions.presenceOfElementLocated
+//				(ByXPath.xpath("//svg[@aria-label='Notifications']")));
+//		
+//		notificationElement.click();
+		
+		
+//		get post parent element	
+		
+		WebElement postElementParent=wait.until(ExpectedConditions.
+				presenceOfElementLocated(
+						ByXPath.xpath("//div[contains(@class,'_ac7v  _al3n')]")));
+		List<WebElement> childElements=postElementParent.findElements(By.tagName("div"));
+		System.out.println("postElement"+childElements.get(0));
+		childElements.get(0).click();
+		
+		
+//		click like list
+		WebElement element=wait.until(ExpectedConditions.
+				presenceOfElementLocated(
+						ByXPath.xpath("//section[contains(@class,'_ae5m _ae5n')]")));
+		element.click();
+//		
+//		WebElement nextElement=wait.until(ExpectedConditions.presenceOfElementLocated(ByXPath.
+//				xpath("(//div[@class='_aarf'])[1]")));
+//		
+		List<WebElement> parentElement=driver.findElements(ByXPath.xpath("(//div[contains(@class,'x1dm5mii x16mil14')])[1]"));
+		ArrayList<String> data=new ArrayList<String>();
+		
+		for(WebElement eachParent:parentElement) {
+			System.out.println(eachParent.getSize());
+			List<WebElement> result=driver.findElements(ByXPath.xpath("(//a[contains(@class,'x1i10hfl x1qjc9v5')])[2]"));
+			for (WebElement eachElement:result) {
+				String value=eachElement.getAttribute("href");
+				data.add(value);
+			}
+		}
+		System.out.println(data);
+		
+
+//		System.out.println(eachPostElementLikeList);
+//		eachPostElementLikeList.click();
+//		
+//		if (eachPostElementLikeList.isEnabled()) {
+//			eachPostElementLikeList.click();
+//		}
+//		else {
+//			System.out.println("like element is not found");
+//		}
+		
+//		for (WebElement eachElement:childElements) {
+//			eachElement.click();
+//			System.out.println(eachElement+"click");
+//		}
+//		
+//		driver.findElement(ByXPath.xpath("//div[contains(@class,'_ac7v ')]//div)[1]")).click();
+		
+		
+//		List<WebElement> postElements=driver.findElements(ByXPath.xpath("//div[contains(@class,'_aabd _aa8k')]"));
+//		System.out.println("postElement"+postElements);
+//		for (WebElement eachElement:postElements) {
+//			eachElement.click();
+//			System.out.println(eachElement+"click");
+//		}
 //		logout operation
 //		href="/anusuya_balakrishnan7/"
 //		String userHref="/".concat(username.concat("/"));
